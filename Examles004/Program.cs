9a8cc638584ac6797676acd9c3283fd53a3b6d37
Maxim Volkov <vlkvmaksim69@yandex>
2023-02-22 01:39:19 +0300

    Добавили 4 проект

--- Examles004/Program.cs
+++ Examles004/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
