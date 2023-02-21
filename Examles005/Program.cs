2d0aca00dbfbdbbcd9d25a3617e4923841f36a6f
Maxim Volkov <vlkvmaksim69@yandex>
2023-02-22 02:21:05 +0300

    Добавили таня

---
+++ Examles005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "таня")
+{
+    Console.WriteLine("Ура, мы так долго тебя ждали");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
