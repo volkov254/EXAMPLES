48bd8cb0ac72dbb45cdf67768558bdff5ea56dcd
Maxim Volkov <vlkvmaksim69@yandex>
2023-02-25 15:16:27 +0300

    Добавили метод массива

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = { 1, 454, 4, 54, 544545, 4711, 721, 4 }; 
+
+int n = array.Length;
+int find = 4; 
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прервать;
+    }
+    // index = index + 1;
+    index++;
+}
