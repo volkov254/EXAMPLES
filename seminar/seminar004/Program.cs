0122aebd8f9e822753083e6b04d32e0282aaf094
Maxim Volkov <vlkvmaksim69@yandex>
2023-03-06 21:11:42 +0300

    Добавили задача семира 4

---
+++ seminar/seminar004/Program.cs
@@ -0,0 +1,55 @@
+﻿/*/// Ввод (Input)
+int num = GetNumberFromUser("Введите целое число А:", "Ошибка ввода!");
+/// логика
+int sumNumbers = GetSumNumbers(num);
+//// Вывод (Output)
+Console.WriteLine($"{num} -> {sumNumbers}");
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect && userNumber >= 0 )
+           return userNumber;
+        Console.WriteLine(errorMessage);   
+    }
+}    
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    while(number > 0 )
+    {
+        sum += number;
+        number--;
+    }
+    return sum;
+}*/
+int num = GetNum("ввведите число", "Ошибка");
+int result = GetSumNumbers(num);
+Console.WriteLine($"{num} -> {result}");
+int GetNum(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect) ///&& userNumber >= 0 )
+           return userNumber;
+        Console.WriteLine(errorMessage);   
+    }
+}    
+
+int GetSumNumbers(int number)
+{
+    if(number < 0 ) number *= -1;
+    int count = 0;
+    if(number == 0) count = 1;
+    while(number > 0 )
+    {
+        count++;
+        number/= 10;// number = number / 10;
+    }
+    return count;
+}
