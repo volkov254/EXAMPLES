23a4192295254a2d0e83665e78dc753da0ed22db
Maxim Volkov <vlkvmaksim69@yandex>
2023-03-06 23:27:09 +0300

    Добавил  задание Семинара №4

--- seminar/seminar004/Program.cs
+++ seminar/seminar004/Program.cs
@@ -26,7 +26,8 @@ int GetSumNumbers(int number)
     }
     return sum;
 }*/
-int num = GetNum("ввведите число", "Ошибка");
+//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+/*int num = GetNum("ввведите число", "Ошибка");
 int result = GetSumNumbers(num);
 Console.WriteLine($"{num} -> {result}");
 int GetNum(string message, string errorMessage)
@@ -52,4 +53,60 @@ int GetSumNumbers(int number)
         number/= 10;// number = number / 10;
     }
     return count;
+}*/
+/////Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+/*int num = GetNumberFromUser("Введите целое число А:", "Ошибка ввода!");
+/// логика
+int result = GetMultiplication(num);
+//// Вывод (Output)
+Console.WriteLine($"{num} -> {result}");
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect && userNumber > 0 )
+           return userNumber;
+        Console.WriteLine(errorMessage);   
+    }
+}    
+
+int GetMultiplication(int number)
+{
+    int sum = 1;
+    while(number > 0 )
+    {
+        sum *= number;
+        number--;
+    }
+    return sum;
+}*/
+///Задача 30: Напишите программу, которая выводит массив из 
+///8 элементов, заполненный нулями и единицами в случайном порядке.
+///[1,0,1,1,0,1,0,0]
+int[] array = InputArray();
+///PrintArray(array);
+Console.WriteLine($"[{String.Join(",", array)}]");//// строчка 89 и 90 одинаковая
+static int [] InputArray()
+{
+
+    int [] arr = new int [8];
+    for(int i = 0; i < 8; i++)
+    {
+         arr[i] = new Random().Next(0,2);
+    }
+   return arr;
 }
+static void PrintArray(int [] array)
+{
+    int length = array. Length;
+    Console.Write("[");
+     for(int i = 0; i < length; i++)
+     {
+        Console.Write($"{array[i]},");
+     }
+    Console.WriteLine("]");
+}
+
+///Math.pow.
